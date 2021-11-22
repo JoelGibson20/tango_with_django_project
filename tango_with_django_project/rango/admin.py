64badfc931ffa4f23e7842173698702b074a0f86
j.c.gibson1@newcastle.ac.uk <ThisIsMyPassword0001>
2021-11-22 15:46:38 +0000

    Created category template and allowed url navigation using slugs.

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-# Register your models here.
-admin.site.register(Category)
-
 
 class PageAdmin(admin.ModelAdmin):
     fields = ["title", "category", "url"]
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+# Register your models here.
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
