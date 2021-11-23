1b80b508c3fc9a9c982e4952232cbd0cc10dee79
j.c.gibson1@newcastle.ac.uk <ThisIsMyPassword0001>
2021-11-23 14:06:11 +0000

    Completed Models,templates, and views section of Tango with Django

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -4,10 +4,13 @@ from rango.models import Category
 from rango.models import Page
 
 def index(request):
+    context_dict = {}
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
