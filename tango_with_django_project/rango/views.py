3cf5d8750233428ebd4e1a94622c6cd42f47be11
JoelGibson20 <joel.gibson20@gmail.com>
2021-12-05 13:42:22 +0000

    Added visits counter to index.html to visually display the number of visits to the index.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -9,8 +9,8 @@ from datetime import datetime
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': page_list}
-
+    visits = int(request.COOKIES.get('visits', '1'))
+    context_dict = {'categories': category_list, 'pages': page_list, "visits": visits }
     response = render(request, 'rango/index.html', context=context_dict)
 
     visitor_cookie_handler(request,response)
