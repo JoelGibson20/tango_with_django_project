deb8cb9fcb1d58883e6fc9701e4dbc2961b81181
JoelGibson20 <joel.gibson20@gmail.com>
2021-12-10 17:27:39 +0000

    Commented population script populate_rango.py

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,4 +1,5 @@
-# Population script, can be used in unit testing to test the database
+# Population script, creates some values and stores them in the database
+# Can be used in unit testing to test the database
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
@@ -6,15 +7,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.setti
 import django
 
 django.setup()
-from rango.models import Category, Page
+from rango.models import Category, Page  # Import our database models
 
 
 def populate():
-    # First, we will create lists of dictionaries containing the pages
-    # we want to add into each category.
-    # Then we will create a dictionary of dictionaries for our categories.
-    # This might seem a little bit confusing, but it allows us to iterate
-    # through each data structure, and add the data to our models.
+    # Create lists of dictionaries containing the pages we want to add into each category.
 
     python_pages = [{"title": "Official Python Tutorial",
                      "url": "http://docs.python.org/2/tutorial/",
@@ -45,25 +42,16 @@ def populate():
          "url": "http://flask.pocoo.org",
          "views": 20}]
 
+    # Create a dictionary of dictionaries for our categories.
     cats = {"Python": [{"pages": python_pages}, 128, 64],
             "Django": [{"pages": django_pages}, 64, 32],
             "Other Frameworks": [{"pages": other_pages}, 32, 16]}
 
-    # If you want to add more categories or pages,
-    # add them to the dictionaries above.
-
-    # The code below goes through the cats dictionary, then adds each category,
-    # and then adds all the associated pages for that category.
-    # if you are using Python 2.x then use cats.iteritems() see
-    # http://docs.quantifiedcode.com/python-anti-patterns/readability/
-    # for more information about how to iterate over a dictionary properly.
-
-    for cat, cat_data in cats.items():
-        c = add_cat(cat, cat_data[1], cat_data[2])
+    for cat, cat_data in cats.items():  # Iterate through the category dictionaries in cats dictionary
+        c = add_cat(cat, cat_data[1], cat_data[2])  # Add category to database with the category's fields
         for p in cat_data[0].items():
-            for q in p[1]:
-                add_page(c, q["title"], q["url"], q["views"])
-
+            for q in p[1]:  # Iterate through the pages in the category dictionary
+                add_page(c, q["title"], q["url"], q["views"])  # Add the pages to the database
 
     # Print out the categories we have added.
     for c in Category.objects.all():
@@ -71,7 +59,7 @@ def populate():
             print("- {0} - {1}".format(str(c), str(p)))
 
 
-def add_page(cat, title, url, views):
+def add_page(cat, title, url, views):  # Method that saves a page to the database
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
     p.views = views
@@ -79,7 +67,7 @@ def add_page(cat, title, url, views):
     return p
 
 
-def add_cat(name, views, likes):
+def add_cat(name, views, likes):  # Method that saves a category to the database
     c = Category.objects.get_or_create(name=name)[0]
     c.views = views
     c.likes = likes
@@ -87,7 +75,7 @@ def add_cat(name, views, likes):
     return c
 
 
-# Start execution here!
+# Execution started here
 if __name__ == '__main__':
     print("Starting Rango population script...")
     populate()
